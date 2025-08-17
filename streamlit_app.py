92ad506c2e3af57748ea9e2d30cde2b4a1c99091
katelynnkim-debug <katelynn.kim@berkeley.edu>
2025-08-16 22:13:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.title("CO₂ Dashboard (Notebook Preview) 🌍")
 st.write("The Republic of Korea's CO₂ Emissions Change in Time and Compared with the Rest of the World")
 
 image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/Top10t.png"
-st.image(image_url, caption="Top 10 CO2 Emission Producing Countries", use_column_width=True)
+st.image(image_url, caption="Top 10 CO2 Emission Producing Countries", use_container_width=True)
