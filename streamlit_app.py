df06b0d747749b508b8977090a3751e50a3594d0
katelynnkim-debug <katelynn.kim@berkeley.edu>
2025-08-16 22:09:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,12 @@ import os
 st.title("CO₂ Dashboard (Notebook Preview) 🌍")
 st.write("The Republic of Korea's CO₂ Emissions Change in Time and Compared with the Rest of the World")
 
-image_path = "plots"
-if os.path.exists(image_path):
-    image = Image.open(image_path)
-    st.image(image, caption = "Sample Plot", use_column_width = True)
+image_name = st.text_input("Top10t.png", "CO2_world.png", "CO2_temp_sk.png", "CO2_temp_sk_scaled.png", "CO2_temp_sk_faceted.png")
 
-else:
-    st.warning(f"Image not found at {image_path}")
+if image_name:
+    image_path = os.path.join("images", image_name)
+    if os.path.exists(image_path):
+        image = Image.open(image_path)
+        st.image(image, caption=f"Loaded: {image_name}", use_column_width=True)
+else: 
+    st.error("Image '{image_name}' not found in the 'images' folder.")
