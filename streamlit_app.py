1951e4fea94248281971c1b143c9c4f3e2571612
katelynnkim-debug <katelynn.kim@berkeley.edu>
2025-08-16 21:49:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 st.title("CO₂ Dashboard (Notebook Preview) 🌍")
 st.write("The Republic of Korea's CO₂ Emissions Change in Time and Compared with the Rest of the World")
 
-image_path = "plots/plot1.png"
+image_path = "plots"
 if os.path.exists(image_path):
     image = Image.open(image_path)
     st.image(image, caption = "Sample Plot", use_column_width = True)
