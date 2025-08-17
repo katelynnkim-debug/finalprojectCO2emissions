1318f75d571d5e7016f175364e6ddfa761aa34c3
katelynnkim-debug <katelynn.kim@berkeley.edu>
2025-08-17 00:58:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ st.write("The Republic of Korea's CO₂ Emissions Change in Time and Compared wi
 
 image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/Top10t.png?raw=true"
 st.image(image_url, caption="Top 10 CO2 Emission Producing Countries", use_container_width=True)
+st.write("The dark blue color at the start of South Korea's plot at 1945 and its gradual blend into a lighter greenish yellow color as the year approaches 2014 clearly shows that the country only started emitting large amounts of CO2 emissions in 1945, towards the end of World War II, and started a steady incline of even more copious amounts of CO2 emissions until 2014, where South Korea has now caught up to most of the other top 10 CO2 emission-producing countries, as seen by the common shade of lighter greenish yellow.")
 
 image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/CO2_world.png?raw=true"
 st.image(image_url, caption="World CO2 Emissions Per Year (1751-2014)", use_container_width=True)
