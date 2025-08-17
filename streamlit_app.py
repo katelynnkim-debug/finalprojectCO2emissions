a894ca7f834f1149941cb3d1c1441a2a295596b0
katelynnkim-debug <katelynn.kim@berkeley.edu>
2025-08-16 21:46:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
-streamlit run app.py
+import streamlit as st
+
+st.title("CO₂ Dashboard (Notebook Preview) 🌍")
+st.write("The Republic of Korea's CO₂ Emissions Change in Time and Compared with the Rest of the World")
+
+image_folder = "plots"
+image_files = [f for f in os.listdir(image_folder) if f.endswith(".png")]
+
+for image_file in image_iles:
+    image_path = os.path.join(image_folder, image_file)
+    image = Image.open(image.path)
+
+    st.subheader(f"Plot: {image_file}")
+    st.image(image, use_column_width=True)
