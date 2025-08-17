711c186fea48f64979740a0b5c0668c2e1b745e4
katelynnkim-debug <katelynn.kim@berkeley.edu>
2025-08-16 22:21:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,12 @@ st.write("The Republic of Korea's CO₂ Emissions Change in Time and Compared wi
 
 image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/Top10t.png?raw=true"
 st.image(image_url, caption="Top 10 CO2 Emission Producing Countries", use_container_width=True)
+
+image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/CO2_world.png?raw=true"
+st.image(image_url, caption="World CO2 Emissions Per Year (1751-2014)", use_container_width=True)
+
+image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/CO2_temp_sk_scaled.png?raw=true"
+st.image(image_url, caption="South Korea's Emissions and Temperatures (1980-2014) - Linear Regression Plot", use_container_width=True)
+
+image_url = "https://github.com/katelynnkim-debug/finalprojectCO2emissions/blob/main/CO2_temp_sk_facet.png?raw=true"
+st.image(image_url, caption="South Korea's Emissions and Temperatures (1980-2014) - Facet Plot", use_container_width=True)
